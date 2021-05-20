d92fce826a2de3c22ad7a34eb6b44321c885ab7a
Juan Vazquez <ehulises28@hotmail.com>
2021-05-19 17:07:08 -0700

    added a jumbotron

---
+++ src/components/JumboTron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function JumboTron() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Employee Directory</h1>
+        <p class="lead">
+          Click on name to filter alphabetically or use the search bar to narrow
+          your results.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default JumboTron;
