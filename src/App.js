6a18d4c375bfc34876925ade71a85b0b5f6cf528
Juan Vazquez <ehulises28@hotmail.com>
2021-05-25 16:22:10 -0700

    added new folders for componenets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import Table from "./components/Table";
-import TableRow from "./components/TableRow";
-import SearchForm from "./components/SearchForm";
+import Header from "./components/Header/Header";
+import Table from "./components/Table/Table";
+import TableRow from "./components/TableRow/TableRow";
+import SearchForm from "./components/SearchForm/SearchForm";
 import API from "./utils/API";
 class App extends Component {
   state = {
